76eaba3c53323ae4bdeec11cb5643dc547175d6a
Elisa97 <elisa.kluge@t-online.de>
2020-08-07 16:02:52 +0200

    camera and fileload added

---
+++ framework/camera.hpp
@@ -0,0 +1,22 @@
+#ifndef CAMERA_HPP
+#define CAMERA_HPP
+
+// External libraries
+#include <glm/vec3.hpp>
+
+// Standard libraries
+#include <string>
+#include <fstream>
+#include <sstream>
+
+
+struct Camera {
+
+    std::string name = "";
+    float fov_x = 60.0;  // between 0 and 180
+    glm::vec3 position = {0.0f, 0.0f, 0.0f};
+    glm::vec3 direction = {0.0f, 0.0f, -1.0f};
+
+    //Ray shoot_ray () Methode implementieren?
+}
+#endif
