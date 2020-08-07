513cb9efadc6a9f9b2ecb8fec0e4926490390c3f
jreee <jeremias-kilian@web.de>
2020-08-07 15:34:35 +0200

    added light and glm

---
+++ framework/light.hpp
@@ -0,0 +1,15 @@
+#ifndef LIGHT_HPP
+#define LIGHT_HPP
+
+#include "color.hpp"
+#include <glm/vec3.hpp>
+
+struct light
+{
+    std::string name = "light0";
+    float brightness = 1.0f;
+    Color lcol = {0.5f, 0.5f, 0.5f};
+    glm::vec3 lloc = {10.0f, 0.0f, 0.0f};
+};
+
+#endif
