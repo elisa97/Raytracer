10a9dfe256730c64f1c826b1631b25440476f6ab
Elisa97 <elisa.kluge@t-online.de>
2020-08-13 10:34:38 +0200

    camera.cpp updated again

--- framework/camera.cpp
+++ framework/camera.cpp
@@ -7,11 +7,11 @@
 Camera::Camera(float fov = 50.0, glm::vec3 const& pos = glm::vec3(0.0, 0.0, 0.0)) :
     fov_h(fov), position(pos) {
         float half_angle_h = fov_h * BUW_MATH_PI / (2.0 * 180);
-        dist_to_image_plane = 0.5 / std::tan(half_angle_h);
+        float dist_to_image_plane = 0.5 / std::tan(half_angle_h);
     }
 
 void Camera::generate_eye_ray(float x, float y, Ray& out_ray) const {
-    auto ray_direction = glm::vec3(x, y, -dist_to_image_plain) - position;
+    auto ray_direction = glm::vec3(x, y, -dist_to_image_plane) - position;
 
     out_ray.origin = position;
     out_ray.direction = ray_direction;
