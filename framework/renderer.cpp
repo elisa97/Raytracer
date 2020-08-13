f93132f94b18bf258ef3ce0c82f7cb4a12f01a33
Elisa97 <elisa.kluge@t-online.de>
2020-08-13 18:45:43 +0200

    render_test update, still many bugs

--- framework/renderer.cpp
+++ framework/renderer.cpp
@@ -25,13 +25,17 @@ void Renderer::render(Scene const& current_scene)
   for (unsigned y = 0; y < height_; ++y) {
     for (unsigned x = 0; x < width_; ++x) {
 
-      //Ray current_eye_ray;
+    Ray current_eye_ray {{}, {x / float(width_) -0.5f, aspect_ratio * (y / float(height_) -0.5f), 0}};
 
-      //HitPoint closest_hit = generate_eye_ray(x / float(width_) -0.5f, aspect_ratio * (y / float(height_) -0.5f), current_eye_ray);
-
+     // HitPoint closest_hit = generate_eye_ray(x / float(width_) -0.5f, aspect_ratio * (y / float(height_) -0.5f), current_eye_ray);
+      auto test = &current_scene.objects.front();
+      auto non_ptr_test = test->get();
+      HitPoint test_hp = non_ptr_test->intersect(current_eye_ray);
 
       Pixel p(x,y);
-      if ( ((x/checker_pattern_size)%2) != ((y/checker_pattern_size)%2)) {
+      if (test_hp.cut) {
+        p.color = Color{test_hp.material->ka.r, test_hp.material->ka.g, test_hp.material->ka.b};
+      } else if ( ((x/checker_pattern_size)%2) != ((y/checker_pattern_size)%2)) {
         p.color = Color{0.0f, 1.0f, float(x)/height_};
       } else {
         p.color = Color{1.0f, 0.0f, float(y)/width_};
--- source/render_test.cpp
+++ source/render_test.cpp
@@ -3,8 +3,13 @@
 
 int main(int argc, char* argv[]){
     Scene new_scene;
-    Renderer new_renderer(600, 400, "output.ppm");
-    
+    Renderer new_renderer{600, 400, "output.ppm"};
+    Material new_material{};
+    std::shared_ptr<Material> mat_ptr = new_material;
+    Sphere new_sphere{{-2, 0, 0}, 0.5, mat_ptr};
+    std::shared_ptr<Sphere> sphere_ptr = new_sphere ;
+    new_scene.objects.push_back(sphere_ptr);
+
     new_renderer.render(new_scene);
 
     return 0;
