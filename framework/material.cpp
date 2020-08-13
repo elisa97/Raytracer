784c0963ba36db94b1b370c3cc5a1cc9262c8b3a
jreee <jeremias-kilian@web.de>
2020-08-13 15:02:33 +0200

    adapted the material hpp to the change

--- framework/material.cpp
+++ framework/material.cpp
@@ -1,13 +1,13 @@
-#include "Material.hpp"
+#include "material.hpp"
 
 std::ostream& Material::print(std::ostream& os)
 {
-os  << "name: " << name_ << "\n" 
-    << "ka: " << ka_ << "\n"
-    << "kd: " << kd_ << "\n"
-    << "ks: " << ks_ << "\n"
-    << "spectular: " << m_ << "\n";
-    return os;
+os  << "name: " << name << "\n" 
+    << "ka: " << ka << "\n"
+    << "kd: " << kd << "\n"
+    << "ks: " << ks << "\n"
+    << "spectular: " << m << "\n";
+  return os;
 }
 
 std::ostream& operator<<(std::ostream& os, Material& material)
@@ -17,7 +17,7 @@ std::ostream& operator<<(std::ostream& os, Material& material)
 
 bool operator ==(Material const& lhs, Material const& rhs)
 {
-  if(lhs.name_.compare(rhs.name_) == 0)
+  if(lhs.name.compare(rhs.name) == 0)
   {
     return true;
   }
