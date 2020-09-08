25ae8b70461e4a75fd4cdffaeacacebdc66a55e0
Elisa97 <elisa.kluge@t-online.de>
2020-09-08 13:55:10 +0200

    styled box and ray

--- framework/ray.hpp
+++ framework/ray.hpp
@@ -6,8 +6,8 @@
 
 struct Ray
 {
-  glm::vec3 origin = {0.0f, 0.0f, 0.0f};
-  glm::vec3 direction = {0.0f, 0.0f, -1.0f};
+  glm::vec3 origin {0.0f, 0.0f, 0.0f};
+  glm::vec3 direction {0.0f, 0.0f, -1.0f};
 };
 
 
