d1e776124e68d925beae3d4cf3079809171a65e4
jreee <jeremias-kilian@web.de>
2020-09-11 15:03:12 +0200

    added a simple shellscript to render animations

---
+++ source/basic_anim.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+
+cd ../build
+make
+clear
+cap=3;
+i=0;
+file="../source/scene_test.sdf"
+ch=5
+
+while [ $i -le $cap ]
+do
+  #navigatig to the sdf
+  new_ch=$(($i * $ch))
+  replace="s/transform eye translate./*/transform eye translate 0 0 "$new_ch"/"
+  echo $replace
+  sed -i replace $file
+  echo 'rendering' $i ' from ' $cap
+  ./source/scene_load_test
+  if [ $i -ge 1000 ]; then
+    name="frame"$i".png"
+    convert test1.ppm $name
+  elif [ $i -ge 100 ]; then
+    name="frame0"$i".png"
+    convert test1.ppm $name
+  elif [ $i -ge 10 ]; then
+    name="frame00"$i".png"
+    convert test1.ppm $name
+  else
+    name="frame000"$i".png"
+    convert test1.ppm $name
+  fi
+  ((i++))
+done
+
+mkdir anim
+mv frame*.png anim
+cd anim 
+ffmpeg -r 24 -i 'frame%04d.png' anim_$cap.mp4
