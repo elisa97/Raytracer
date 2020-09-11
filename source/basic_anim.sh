4ca6319b4ab0f42618223e26df3424a3ae9b91b1
jreee <jeremias-kilian@web.de>
2020-09-12 00:23:54 +0200

    cleaned up the bash script

--- source/basic_anim.sh
+++ source/basic_anim.sh
@@ -1,37 +1,40 @@
 #! /bin/bash
 
+#path to the sdf
+file="../source/scene_test.sdf"
+
+cap=120
+i=0
+ch=1
+
 cd ../build
 make
 clear
-cap=3;
-i=0;
-file="../source/scene_test.sdf"
-ch=5
+
+if [ ! -d "$anim" ]; then
+  echo 'created folder anim'
+  mkdir anim
+fi
 
 while [ $i -le $cap ]
 do
-  #navigatig to the sdf
   new_ch=$(($i * $ch))
-  sed -i 's/transform eye translate.*/transform eye translate 0 0 '$new_ch'/' $file
+
+  #rewriting a line in the file
+  sed -i 's/transform eye rotate.*/transform eye rotate '$new_ch' 0 1 0/' $file
+
+  #rendering the file
   echo 'rendering' $i ' from ' $cap
   ./source/scene_load_test
-  if [ $i -ge 1000 ]; then
-    name="frame"$i".png"
-    convert test1.ppm $name
-  elif [ $i -ge 100 ]; then
-    name="frame0"$i".png"
-    convert test1.ppm $name
-  elif [ $i -ge 10 ]; then
-    name="frame00"$i".png"
-    convert test1.ppm $name
-  else
-    name="frame000"$i".png"
-    convert test1.ppm $name
-  fi
+
+  #padding for ffmpeg
+  convert test1.ppm 'anim/frame'`printf "%04d" $i`'.png'
+  
   ((i++))
 done
 
-mkdir anim
-mv frame*.png anim
-cd anim 
-ffmpeg -r 24 -i 'frame%04d.png' anim_$cap.mp4
+#generating the video
+ffmpeg -r 30 -i anim/'frame%04d.png' anim_$cap.mp4
+
+#cleanup if needed
+#rm anim
